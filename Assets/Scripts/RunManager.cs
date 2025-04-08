56b75972f7475044c0c7ebddd22b708dfcacd297
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-04-08 14:23:49 +0900

    t

---
+++ Assets/Scripts/RunManager.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RunManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI[] SUPER_Text;
+
+
+    
+    void Start()
+    {
+        //S_Text.fontMaterial.SetColor("_UnderlayColor", new Color(0f,0f,0f,0f));
+
+
+    }
+
+   
+    void Update()
+    {
+
+
+
+        
+    }
+}
