f065e69c34d713d8364ba153a42bf6ce0d632778
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-04-08 13:45:37 +0900

    t

--- Assets/Scripts/Test3.cs
+++ Assets/Scripts/Test3.cs
@@ -3,23 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class Test3 : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject Test;
     
-
+  
 
     void Start()
     {
         SceneManager.LoadSceneAsync("Test_1", LoadSceneMode.Additive);
         SceneManager.LoadSceneAsync("Test_2", LoadSceneMode.Additive);
     }
-
-    private void Update()
-    {
-        
-
-
-    }
-
 
 }
